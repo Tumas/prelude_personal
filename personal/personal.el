ab7c218eb133a2972896913c2030e6d405d0dcbe
Tumas Bajoras <tumas.bajoras@necolt.com>
2015-07-16 14:56:05 +0300

    require evil mode & use wombat as default colorscheme

--- personal/personal.el
+++ personal/personal.el
@@ -3,3 +3,15 @@
 
 ;; show line numbers
 (global-linum-mode t)
+
+;; enable column number mode
+(column-number-mode t)
+
+;; enable evil mode
+(require 'prelude-evil)
+
+;; move to splits with a letter
+(global-set-key (kbd "C-l") 'windmove-right)
+(global-set-key (kbd "C-k") 'windmove-up)
+(global-set-key (kbd "C-j") 'windmove-down)
+(global-set-key (kbd "C-h") 'windmove-left)
