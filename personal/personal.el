40c4d72594afd49315e82a9acc3d15d405e621a5
Tumas Bajoras <tumas.bajoras@necolt.com>
2015-07-20 12:49:45 +0300

    some tweaks + add rspec-mode

--- personal/personal.el
+++ personal/personal.el
@@ -7,6 +7,16 @@
 ;; enable column number mode
 (column-number-mode t)
 
+;; dependencies
+(prelude-require-packages '(solarized-theme))
+(prelude-require-packages '(rspec-mode))
+
+;; visuals
+(set-default-font "Monospace 9")
+
+(disable-theme 'zenburn)
+(setq prelude-theme 'solarized-dark)
+
 ;; enable evil mode
 (require 'prelude-evil)
 
@@ -15,3 +25,10 @@
 (global-set-key (kbd "C-k") 'windmove-up)
 (global-set-key (kbd "C-j") 'windmove-down)
 (global-set-key (kbd "C-h") 'windmove-left)
+
+;; if indent-tabs-mode is off, untabify before saving
+(add-hook 'write-file-hooks
+  (lambda ()
+    (if (not indent-tabs-mode)
+      (untabify (point-min) (point-max)))
+    nil ))
